fe1be6b7e70dd3d1a061e73599180235271ce60f
Manweill <manweill@outlook.com>
2023-12-27 17:34:07 +0800

    房间传感器展示

--- src/views/dashboard/workplace/components/categories-percent.vue
+++ src/views/dashboard/workplace/components/categories-percent.vue
@@ -2,13 +2,61 @@
   <a-spin :loading="loading" style="width: 100%">
     <a-card
       class="general-card"
-      :header-style="{ paddingBottom: '0' }"
-      :body-style="{
-        padding: '20px',
-      }"
+      :header-style="{ paddingBottom: '0', textAlign: 'center' }"
     >
-      <template #title> 设备接入统计 </template>
-      <Chart height="310px" :option="chartOption" />
+      <template #title> 实时监控 </template>
+      <a-grid :cols="24" :col-gap="16" :row-gap="16" style="margin-top: 16px">
+        <a-grid-item
+          v-for="(id, idx) in items"
+          :key="id"
+          :span="{ xs: 12, sm: 12, md: 12, lg: 8, xl: 8, xxl: 8 }"
+        >
+          <!-- <a-card :title="idx + '#房'">
+            <template #extra>
+              <a-link>查看</a-link>
+            </template>
+            ByteDance's core product, Toutiao ("Headlines"), is a content
+            platform in China and around the world. Toutiao started out as a
+            news recommendation engine and gradually evolved into a platform
+            delivering content in various formats.
+          </a-card> -->
+          <a-card>
+            <div style="display: flex; justify-content: space-between">
+              <a-link>{{ idx + 1 + '#房' }}</a-link>
+              <span>{{ idx + 1 }}</span>
+            </div>
+            <a-list-item
+              v-for="(item, i) in ['温度传感器', '湿度传感器']"
+              :key="i"
+            >
+              <a-list-item-meta :title="item">
+                <template #avatar>
+                  <a-avatar shape="square">
+                    <img alt="avatar" :src="thpImage" />
+                  </a-avatar>
+                </template>
+              </a-list-item-meta>
+              <template #extra>
+                <div
+                  style="
+                    display: flex;
+                    align-items: center;
+                    font-size: 28px;
+                    height: 100%;
+                  "
+                >
+                  35 °C
+                </div>
+              </template>
+            </a-list-item>
+          </a-card>
+        </a-grid-item>
+        <!-- <a-grid-item
+          :span="{ xs: 24, sm: 24, md: 24, lg: 12, xl: 12, xxl: 12 }"
+        >
+          <CategoriesPercent />
+        </a-grid-item> -->
+      </a-grid>
     </a-card>
   </a-spin>
 </template>
@@ -16,7 +64,9 @@
 <script lang="ts" setup>
   import useLoading from '@/hooks/loading';
   import useChartOption from '@/hooks/chart-option';
+  import thpImage from '@/assets/images/Temperature_Humidity_Profile.png';
 
+  const items = [1, 2, 3, 4];
   const { loading } = useLoading();
   const { chartOption } = useChartOption((isDark) => {
     // echarts support https://echarts.apache.org/zh/theme-builder.html
